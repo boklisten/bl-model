8fcd0e7501310fca4f0d69d10875e7057c787396
holskil <aholskil@gmail.com>
2017-11-25 16:39:17 +0100

    feat: OpeningHour class
    the OpeningHour class is used to tell which opening hours a specific
    branch has.

---
+++ src/opening-hour/opening-hour.ts
@@ -0,0 +1,11 @@
+
+
+export class OpeningHour {
+	id: string;
+	from: Date;
+	to: Date;
+	branch: string;
+	active: boolean;
+	creationTime: Date;
+	lastUpdated: Date;
+}
--- src/public-api.ts
+++ src/public-api.ts
@@ -1,7 +1,5 @@
-
 export {Item} from './item/item';
 export {Branch} from './branch/branch';
 export {BlapiResponse} from './blapi-response/blapi-response';
 export {BlapiErrorResponse} from './blapi-response/blapi-error-response';
 export {OpeningHour} from './opening-hour/opening-hour';
-
