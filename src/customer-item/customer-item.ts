c736c124fcc30d9ab6c0e6b63005157d4d75f973
holskil <aholskil@gmail.com>
2018-01-11 16:47:15 +0100

    feat: customerItem class

---
+++ src/customer-item/customer-item.ts
@@ -0,0 +1,23 @@
+import {BlDocument} from "../bl-document/bl-document";
+
+export class CustomerItem extends BlDocument {
+	item: string;
+	deadline: Date;
+	status: string;
+	handout: boolean;
+	handoutTime: boolean;
+	handoutBranch: string;
+	handoutEmployee: string;
+	returned: boolean;
+	returnTime: Date;
+	returnBranch: string;
+	returnEmployee: string;
+	totalAmount: number;
+	orderItems: string[];
+	deadlineExtends: {
+		oldDeadline: Date,
+		newDeadline: Date,
+		orderItem: string,
+		time: Date
+	}[];
+}
--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,4 @@ export {BlapiResponse} from './blapi-response/blapi-response';
 export {BlapiErrorResponse} from './blapi-response/blapi-error-response';
 export {OpeningHour} from './opening-hour/opening-hour';
 export {Order} from './order/order';
+export {Comment} from './comment/comment';
