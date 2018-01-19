4f18ceef3209780c991bc42652d11720c4c16d1e
holskil <aholskil@gmail.com>
2018-01-19 09:39:15 +0100

    fix: orderItem.type cancel is now changed to "cancel-rent"

--- src/order-item/order-item.ts
+++ src/order-item/order-item.ts
@@ -2,7 +2,7 @@
 export class OrderItem {
 	item: string;
 	amount: number;
-	type: "rent" | "buy" | "extend" | "sell" | "buyout" | "cancel" | "cancel-buy" | "cancel-extend" | "cancel-sell" | "cancel-buyout";
+	type: "rent" | "buy" | "extend" | "sell" | "buyout" | "cancel-rent" | "cancel-buy" | "cancel-extend" | "cancel-sell" | "cancel-buyout";
 	customerItem?: string;
 	discount?: number;
 	rentInfo?: {
