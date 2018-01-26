440d1bf839f69b903a2694c827b779589cb6751e
holskil <aholskil@gmail.com>
2018-01-26 16:47:21 +0100

    fix: added som description comments to orderItem

--- src/order-item/order-item.ts
+++ src/order-item/order-item.ts
@@ -2,11 +2,11 @@
 export class OrderItem {
 	item: string;
 	title: string;
-	amount: number;
-	unitPrice: number;
-	taxAmount: number;
-	rentRate: number;
-	taxRate: number;
+	amount: number; //unitPrice + taxAmount
+	unitPrice: number; //item.price * rentRate
+	taxAmount: number; //unitPrice * taxRate
+	rentRate: number; //branch.rentRate(one/two semesters)
+	taxRate: number; //item.taxRate
 	type: "rent" | "buy" | "extend" | "sell" | "buyout" | "cancel-rent" | "cancel-buy" | "cancel-extend" | "cancel-sell" | "cancel-buyout";
 	customerItem?: string;
 	discount?: number;
