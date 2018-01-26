5b26208194d54c6991186719796ecd36cba50859
holskil <aholskil@gmail.com>
2018-01-26 10:50:35 +0100

    fix: orderItem now has a title field

--- src/order-item/order-item.ts
+++ src/order-item/order-item.ts
@@ -1,6 +1,7 @@
 
 export class OrderItem {
 	item: string;
+	title?: string;
 	amount: number;
 	type: "rent" | "buy" | "extend" | "sell" | "buyout" | "cancel-rent" | "cancel-buy" | "cancel-extend" | "cancel-sell" | "cancel-buyout";
 	customerItem?: string;
