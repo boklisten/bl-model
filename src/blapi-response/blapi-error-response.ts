19893cb0f0ca2ceb7070a2cd8d3261c36f26eb07
holskil <aholskil@gmail.com>
2018-02-05 14:23:21 +0100

    fix: blapiErrorResponse now has code and httpStatus

--- src/blapi-response/blapi-error-response.ts
+++ src/blapi-response/blapi-error-response.ts
@@ -1,11 +1,13 @@
 
 
 export class BlapiErrorResponse {
-	code: number;
+	httpStatus: number;
+	code: number | undefined;
 	msg: string | undefined;
 	data: any;
 	
-	constructor(code: number, msg?: string, data?: any) {
+	constructor(httpStatus: number, code?: number, msg?: string, data?: any) {
+		this.httpStatus = httpStatus;
 		this.code = code;
 		this.msg = msg;
 		this.data = data;
