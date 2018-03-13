00678e0854ac5fb09611b471837da459d042fe5f
holskil <aholskil@gmail.com>
2018-03-13 07:45:01 +0100

    fix: now have a permission field under user in BlDocument

--- src/bl-document/bl-document.ts
+++ src/bl-document/bl-document.ts
@@ -1,13 +1,15 @@
 
 import {Comment} from "../comment/comment";
+import {UserPermission} from "../permission/user-permission";
 
 export class BlDocument {
 	id: string;
-	lastUpdated: Date;
-	creationTime: Date;
-	comments: Comment[];
-	active: boolean;
+	lastUpdated?: Date;
+	creationTime?: Date;
+	comments?: Comment[];
+	active?: boolean;
 	user?: {
 		id: string;
+		permission?: UserPermission;
 	}
 }
