6bf076e187ab2d181f1b9e6bd4c8fc4b5812d4fa
holskil <aholskil@gmail.com>
2018-03-19 10:53:53 +0100

    feat: SharedItem
    a sharedItem is a item with a unique id so it can be traced throughout
    the system. This creates the possibility to a "peer-to-peer" system,
    where the customers hands out the items to other customers. It also
    adds the possibility for a branch to hold a inventory.

--- src/branch/branch.ts
+++ src/branch/branch.ts
@@ -4,35 +4,33 @@ import {PaymentMethod} from "../payment/payment-method/payment-method";
 import {BranchPaymentInfo} from "./branch-payment-info";
 
 export class Branch extends BlDocument {
-	name: string;
-	type?: string;
-	desc?: string;
-	root?: boolean;
-	childBranches?: string[];
-	items?: string[];
-	openingHours?: string[];
-	paymentInfo?: BranchPaymentInfo;
-	payment?: {
-		branchResponsible: boolean,
-		rentPricePercentage: {
-			base: number,
-			oneSemester: number,
-			twoSemesters: number,
-			buyout: number
-		},
-		extendPrice: number,
-		acceptedMethods: PaymentMethod[]
-	};
-	itemCategories?: {
-		name: string,
-		items: string[]
-	}[];
-	contactInfo?: {
+	name: string; // the name of the branch
+	type?: string; // the type of branch, ex. School or Storage
+	desc?: string; // the description of the branch
+	root?: boolean; // is this a root branch
+	childBranches?: string[]; // does this branch have child branches
+	openingHours?: string[]; // id of all the opening hours this branch has
+	paymentInfo?: BranchPaymentInfo; // payment information for this branch
+	contactInfo?: { // the contact info for the branch
 		phone: number,
 		email: string,
 		address: string,
 		postCode: string,
 		postCity: string,
-		country: string
+		country: string,
+		locationDesc?: string, // description of how to get to the location, ex: 2nd floor up the elevator to the left
+		location?: { // to place the location of this branch on a map
+			latitude: string,
+			longitude: string
+		}
 	};
+	items?: string[]; // all the items this branch has
+	inventory?: [{ // the inventory of this branch, holds info about how many of each item it has
+		item: string, // the item
+		sharedItems: [string] // a list of ids to sharedItems of type item aka, how many of each
+	}];
+	itemCategories?: [{ // the item categories
+		name: string, // the name of the category
+		items: string[] // the items this category have
+	}];
 }
--- src/customer-item/customer-item.ts
+++ src/customer-item/customer-item.ts
@@ -1,22 +1,29 @@
 import {BlDocument} from "../bl-document/bl-document";
 
+
+/**
+ * A CustomerItem is a item that the customer is renting.
+ * It holds information about the rental period such as the deadline.
+ * it can either be handed out by a Branch or another Customer.
+ */
 export class CustomerItem extends BlDocument {
-	item: string;
-	deadline: Date;
-	handout: boolean;
-	handoutTime?: Date;
-	handoutBranch?: string;
-	handoutEmployee?: string;
-	status?: "rent" | "cancel" | "buy";
-	returned?: boolean;
-	returnTime?: Date;
-	returnBranch?: string;
-	returnEmployee?: string;
-	totalAmount: number;
-	orderItems: string[];
-	deadlineExtends?: {
-		oldDeadline: Date,
-		newDeadline: Date,
-		time: Date
-	}[];
+	item: string; // what item is this customerItem for
+	sharedItem?: string; // can point to a shared item, if so the item has a unique id and can be traced
+	deadline: Date; //the deadline to return this item
+	status?: "rent" | "cancel" | "buy"; // status of the customerItem
+	handout: boolean; // if this customerItem is handed out to customer or not
+	handoutInfo?: {
+		handoutBy: "branch" | "customer", // if this was handed out by another customer or a branch
+		handoutById: string, // the id of the branch or customer that handed out the item
+		handoutEmployee?: string, // if at branch, this is the id of the employee that handed out the item
+		time: Date // the time this item was handed out
+	};
+	returned: boolean; // if this item is returned or not
+	returnInfo?: {
+		returnedTo: "branch" | "customer", //if the item was returned to a branch or a customer
+		returnedToId: string, // if returnedTo a branch, this is the id of a branch, otherwise a customer
+		returnEmployee?: string, // if it was returned to a branch, this is the id of the employee
+		time: Date //the time of return
+	};
+	orders?: string[]; // what orders are this customerItem a part of, must be at least one, the order placement
 }
