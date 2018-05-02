fddaa3bbdee6de286187d2848e3ecaaf64e557b2
holskil <aholskil@gmail.com>
2018-05-02 10:21:47 +0200

    fix: orderItem only has one cancel type

--- src/order/order-item/order-item-type.ts
+++ src/order/order-item/order-item-type.ts
@@ -1,3 +1,2 @@
 
-export type OrderItemType =  "rent" | "buy" | "extend" | "sell" | "buyout" |
-	"cancel-rent" | "cancel-buy" | "cancel-extend" | "cancel-sell" | "cancel-buyout";
+export type OrderItemType =  "rent" | "buy" | "extend" | "sell" | "buyout" | "cancel";
