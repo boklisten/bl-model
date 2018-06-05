12c460116dcc4a283ef2b22f6fff244cf62b8f19
holskil <aholskil@gmail.com>
2018-06-05 14:16:19 +0200

    fix: date is now set in each extendPeriods and rentPeriods

--- src/branch/branch-payment-info.ts
+++ src/branch/branch-payment-info.ts
@@ -5,11 +5,13 @@ export class BranchPaymentInfo {
 	responsible: boolean; // if set the branch is responsible for payment, not the customer
 	rentPeriods: [{
 		type: Period, // the allowed period
+		date: Date,
 		maxNumberOfPeriods: number, // max number of periods of this type one item can be rented
 		percentage: number //the percentage of the item.price the rent price is
 	}];
 	extendPeriods: [{
 		type: Period, //the possible extend period type
+		date: Date,
 		maxNumberOfPeriods: number, // the max number of periods this item can be extended
 		price: number, // the price of the extend period
 		percentage?: number //if set then use percentage of item
