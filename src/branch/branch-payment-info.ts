2323eaef566aee86b3c904e367482f07526e203d
holskil <aholskil@gmail.com>
2018-08-03 17:03:59 +0200

    fix: added a flag to branch to tell if responsible for delivery or not

--- src/branch/branch-payment-info.ts
+++ src/branch/branch-payment-info.ts
@@ -3,6 +3,7 @@ import {Period} from "../period/period";
 
 export class BranchPaymentInfo {
 	responsible: boolean; // if set the branch is responsible for payment, not the customer
+	responsibleForDelivery?: boolean; // if set the customer does not need to pay for the delivery
 	rentPeriods: [{
 		type: Period, // the allowed period
 		date: Date,
