2291844697fd705bdb3fdd05061beb179e8734e7
holskil <aholskil@gmail.com>
2019-01-02 14:41:41 +0100

    fix: renamed deadline to "to" in OrderItemPartlyPaymentInfo

--- src/order/order-item/order-item-partly-payment-info.ts
+++ src/order/order-item/order-item-partly-payment-info.ts
@@ -1,5 +1,6 @@
 
 export type OrderItemPartlyPaymentInfo = {
-  deadline: Date; // the deadline for the next partly payment
+  from?: Date;
+  to: Date; // the deadline for the next partly payment
   amountLeftToPay: number; // the amount left to pay on buyout of this item
 }
