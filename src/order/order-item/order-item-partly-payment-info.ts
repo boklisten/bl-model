fc386f68f9570856f253fcfa34d76a1935c628fc
holskil <aholskil@gmail.com>
2019-01-02 12:16:19 +0100

    fix: added a missing '=' sign

--- src/order/order-item/order-item-partly-payment-info.ts
+++ src/order/order-item/order-item-partly-payment-info.ts
@@ -1,7 +1,7 @@
 import { Period } from "../../period/period";
 import { CustomerItem } from "../../customer-item/customer-item";
 
-export type OrderItemPartlyPaymentInfo {
+export type OrderItemPartlyPaymentInfo = {
   deadline: Date; // the deadline for the next partly payment
   periodType: Period; // what type of period
   customerItem?: string | CustomerItem; // id of customerItem of the CusotmerItem
