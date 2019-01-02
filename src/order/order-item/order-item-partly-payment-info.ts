91081689cf7e3fe5964825e065f0ef04bfa8af25
holskil <aholskil@gmail.com>
2019-01-02 14:45:48 +0100

    fix: added periodType to OrderItemPartlyPaymentInfo

--- src/order/order-item/order-item-partly-payment-info.ts
+++ src/order/order-item/order-item-partly-payment-info.ts
@@ -1,6 +1,9 @@
+import { Period } from "../../period/period";
 
 export type OrderItemPartlyPaymentInfo = {
   from?: Date;
   to: Date; // the deadline for the next partly payment
+  periodType: Period; // the type of period
+  numberOfPeriods?: number;
   amountLeftToPay: number; // the amount left to pay on buyout of this item
 }
