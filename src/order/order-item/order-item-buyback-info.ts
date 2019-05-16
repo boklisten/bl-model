357fe9df49bf64d19ecf4ad3569a490ee836939d
holskil <aholskil@gmail.com>
2019-05-16 11:11:32 +0200

    fix: added missing fields in orderItemTypeBuyback

--- src/order/order-item/order-item-buyback-info.ts
+++ src/order/order-item/order-item-buyback-info.ts
@@ -2,6 +2,10 @@ import {Period} from '../../period/period';
 import {CustomerItem} from '../../customer-item/customer-item';
 
 export type OrderItemBuybackInfo = {
-  buybackAmount: number; // the amount the employee pays to buyback
+  buybackAmount?: number; // the amount the employee pays to buyback
   customerItem?: string | CustomerItem;
+  from?: Date;
+  to?: Date;
+  numberOfPeriods?: number;
+  periodType?: Period;
 };
