17a4dd2e1afbc9e3f244d6c03a088b77e90f515d
holskil <aholskil@gmail.com>
2019-05-24 10:52:27 +0200

    fix: added buyback field in item

--- src/item/item.ts
+++ src/item/item.ts
@@ -1,13 +1,13 @@
-
-import {BlDocument} from "../bl-document/bl-document";
-import {ItemType} from "./item-type";
+import {BlDocument} from '../bl-document/bl-document';
+import {ItemType} from './item-type';
 
 export class Item extends BlDocument {
-	title: string;
-	type: ItemType;
-	price: number;
-	taxRate: number;
-	info?: any;
-	desc?: string;
-	categories?: string[];
+  title: string;
+  type: ItemType;
+  price: number;
+  taxRate: number;
+  info?: any;
+  desc?: string;
+  buyback?: boolean;
+  categories?: string[];
 }
