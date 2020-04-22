773743f1a58527d74587602bd58ec3ce51ef0716
holskil <andreasholskil@protonmail.com>
2020-04-22 10:19:57 +0200

    feat: Booking

---
+++ src/booking/booking.ts
@@ -0,0 +1,8 @@
+import { BlDocument } from "../bl-document/bl-document";
+
+export class Booking extends BlDocument {
+  from: Date;
+  to: Date;
+  customer: string;
+  branch: string;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,55 +1,58 @@
-export {BlDocument} from './bl-document/bl-document';
-export {BlError} from './bl-error/bl-error';
-export {BlapiErrorResponse} from './blapi-response/blapi-error-response';
-export {BlapiResponse} from './blapi-response/blapi-response';
-export {Branch} from './branch/branch';
-export {BranchItem} from './branch-item/branch-item';
-export {Comment} from './comment/comment';
-export {CustomerItem} from './customer-item/customer-item';
-export {CustomerItemType} from './customer-item/customer-item-type';
-export {Item} from './item/item';
-export {Message} from './message/message';
-export {OpeningHour} from './opening-hour/opening-hour';
-export {Order} from './order/order';
-export {OrderItem} from './order/order-item/order-item';
-export {OrderItemType} from './order/order-item/order-item-type';
-export {UserPermission} from './permission/user-permission';
-export {AccessToken} from './token/access-token';
-export {RefreshToken} from './token/refresh-token';
-export {UserDetail} from './user/user-detail/user-detail';
-export {TextBlock} from './text-block/text-block';
-export {Invoice} from './invoice/invoice';
+export { BlDocument } from "./bl-document/bl-document";
+export { BlError } from "./bl-error/bl-error";
+export { BlapiErrorResponse } from "./blapi-response/blapi-error-response";
+export { BlapiResponse } from "./blapi-response/blapi-response";
+export { Booking } from "./booking/booking";
+export { Branch } from "./branch/branch";
+export { BranchItem } from "./branch-item/branch-item";
+export { Comment } from "./comment/comment";
+export { CustomerItem } from "./customer-item/customer-item";
+export { CustomerItemType } from "./customer-item/customer-item-type";
+export { Item } from "./item/item";
+export { Message } from "./message/message";
+export { OpeningHour } from "./opening-hour/opening-hour";
+export { Order } from "./order/order";
+export { OrderItem } from "./order/order-item/order-item";
+export { OrderItemType } from "./order/order-item/order-item-type";
+export { UserPermission } from "./permission/user-permission";
+export { AccessToken } from "./token/access-token";
+export { RefreshToken } from "./token/refresh-token";
+export { UserDetail } from "./user/user-detail/user-detail";
+export { TextBlock } from "./text-block/text-block";
+export { Invoice } from "./invoice/invoice";
 
-export {Match} from './match/match';
-export {MatchState} from './match/match-state';
-export {MatchProfile} from './match/match-profile';
-export {MatchItem} from './match/match-item';
+export { Match } from "./match/match";
+export { MatchState } from "./match/match-state";
+export { MatchProfile } from "./match/match-profile";
+export { MatchItem } from "./match/match-item";
 
-export {Location} from './location/location';
+export { Location } from "./location/location";
 
-export {Payment} from './payment/payment';
-export {PaymentMethod} from './payment/payment-method/payment-method';
-export {PaymentInfo} from './payment/payment-info/payment-info';
-export {Period} from './period/period';
+export { Payment } from "./payment/payment";
+export { PaymentMethod } from "./payment/payment-method/payment-method";
+export { PaymentInfo } from "./payment/payment-info/payment-info";
+export { Period } from "./period/period";
 
-export {Delivery} from './delivery/delivery';
-export {DeliveryMethod} from './delivery/delivery-method/delivery-method';
-export {DeliveryInfoBring} from './delivery/delivery-info/delivery-info-bring';
+export { Delivery } from "./delivery/delivery";
+export { DeliveryMethod } from "./delivery/delivery-method/delivery-method";
 export {
-  DeliveryInfoBranch,
-} from './delivery/delivery-info/delivery-info-branch';
-
-export {BlApiError} from './bl-api-error/bl-api-error';
+  DeliveryInfoBring
+} from "./delivery/delivery-info/delivery-info-bring";
 export {
-  BlApiLoginRequiredError,
-} from './bl-api-error/bl-api-login-required-error';
+  DeliveryInfoBranch
+} from "./delivery/delivery-info/delivery-info-branch";
+
+export { BlApiError } from "./bl-api-error/bl-api-error";
 export {
-  BlApiPermissionDeniedError,
-} from './bl-api-error/bl-api-permission-denied-error';
-export {BlApiNotFoundError} from './bl-api-error/bl-api-not-found-error';
+  BlApiLoginRequiredError
+} from "./bl-api-error/bl-api-login-required-error";
+export {
+  BlApiPermissionDeniedError
+} from "./bl-api-error/bl-api-permission-denied-error";
+export { BlApiNotFoundError } from "./bl-api-error/bl-api-not-found-error";
 
 export {
-  SendgridEvent,
-} from './message/message-sendgrid-event/message-sendgrid-event';
+  SendgridEvent
+} from "./message/message-sendgrid-event/message-sendgrid-event";
 
-export {Company} from './company/company';
+export { Company } from "./company/company";
