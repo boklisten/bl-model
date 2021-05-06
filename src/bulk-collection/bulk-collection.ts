599cc14d644006d28d20853734396aa6561398c8
AdrianAndersen <adrianandersen@protonmail.com>
2021-05-06 08:22:41 +0200

    Add bulk collection types

---
+++ src/bulk-collection/bulk-collection.ts
@@ -0,0 +1,11 @@
+export type ScannedBook = {
+	isbn: string;
+	blid: string;
+	title: string;
+	customerName: string;
+	deadline: string;
+	item: string;
+	id: string;
+	customerId: string;
+	collectedAt?: string;
+};
