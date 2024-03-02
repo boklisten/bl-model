2c32ebd1df1e17caea9aeb6d0df1baf7024ee331
Lars Mitsem Selbekk <lars@selbekk.com>
2024-03-02 17:48:33 +0100

    feat(signature): add num months valid

--- src/signature/serialized-signature.ts
+++ src/signature/serialized-signature.ts
@@ -1,6 +1,8 @@
 import { BlDocument } from "../bl-document/bl-document";
 
 export class SignatureMetadata extends BlDocument {
+  static readonly NUM_MONTHS_VALID = 4 * 12;
+
   signingName: string;
   signedByGuardian: boolean;
 }
